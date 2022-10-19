fe7cc2dacebd752db5fc158b1829f4ca96438f92
Abimbolaomofalana <falanaopeyemi02@gmail.com>
2022-10-18 20:25:16 -0700

    0. _putchar

---
+++ alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ *  ** main - a program that prints _putchar, followed by a new line
+ *
+ *   ** Description: it prints the word _putchar
+ *
+ *    ** Return: ALways 0
+ *
+ *     **/
+
+int main(void)
+
+{
+
+	putchar('');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+
+}
